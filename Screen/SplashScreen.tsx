defdc69cbb3d73a265c7a93ec90cd2744e902400
shashank <singhshashank081@gmail.com>
2025-11-29 12:32:10 +0530

    updated splash screen image

--- Screen/SplashScreen.tsx
+++ Screen/SplashScreen.tsx
@@ -1,10 +1,10 @@
 import React, { useEffect, useRef } from "react";
-import { Animated, Dimensions, ImageBackground, StyleSheet } from "react-native";
+import { Animated, Dimensions, Image, ImageBackground, StyleSheet, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
-const windowWidth = Dimensions.get("window").width;
-const windowHeight = Dimensions.get("window").height;
-export default function SplashScreen({ navigation }) {
 
+const windowHeight = Dimensions.get("window").height;
+const windowWidth = Dimensions.get("window").width;
+export default function SplashScreen({ navigation }) {
   const fadeAnim = useRef(new Animated.Value(0)).current;
 
   useEffect(() => {
@@ -16,33 +16,44 @@ export default function SplashScreen({ navigation }) {
 
     const timeout = setTimeout(() => {
       navigation.replace("Home");
-    }, 4500); // suggested 3 seconds
+    }, 4500);
 
     return () => clearTimeout(timeout);
   }, []);
 
-    return (
-        <SafeAreaView style={{ flex: 1 }} edges={[]}> 
-        <Animated.View style={{ flex: 1, opacity: fadeAnim }}>
-          <ImageBackground
-            source={require("../assets/images/stream4us/logo/stream4us_splash.png")}
-            style={{
-              flex: 1,
-              width: "100%",
-              height: windowHeight
-            }}
-          />
-        </Animated.View>
-      </SafeAreaView>
-      );
+  return (
+    <SafeAreaView style={{ flex: 1 }} edges={[]}>
+      <Animated.View style={{ flex: 1, opacity: fadeAnim }}>
+        <ImageBackground
+          source={require("../assets/images/stream4us/logo/stream4us_splash.png")}
+          style={styles.bg}
+        >
+          <View style={styles.centerContainer}>
+            <Image
+              source={require("../assets/images/stream4us/logo/icon-stream4us.png")}
+              style={styles.centerImage}
+            />
+          </View>
+        </ImageBackground>
+      </Animated.View>
+    </SafeAreaView>
+  );
 }
 
 const styles = StyleSheet.create({
-  container: { flex: 1, backgroundColor: "#0D0E10" },
   bg: {
     flex: 1,
     width: "100%",
     height: windowHeight,
-    resizeMode: "cover",
-  }
+  },
+  centerContainer: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  centerImage: {
+    width: 180,
+    height: 180,
+    resizeMode: "contain",
+  },
 });
