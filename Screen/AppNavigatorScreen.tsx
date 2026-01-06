89e92c8a9f631003db9c4c55fdcb37d4ba180153
shashank <singhshashank081@gmail.com>
2026-01-06 12:20:13 +0530

    udpated navigator

--- Screen/AppNavigatorScreen.tsx
+++ Screen/AppNavigatorScreen.tsx
@@ -50,68 +50,68 @@ export default function AppNavigatorScreen({ route }) {
             headerShown: false,
           }} />
           <Stack.Screen name="Home" component={Home} options={{
-            presentation: "transparentModal",
+            presentation: "card",
             animation: 'fade',
-            gestureEnabled: true,
+            gestureEnabled: false,
             headerLeft: () => null,
             headerShown: false,
             headerStyle: { backgroundColor: "#0D0E10", height: windowHeight / 10 },
             headerTintColor: "#ffffff"
           }} />
           <Stack.Screen name="Action Movies" component={ActionMovies} options={{
-            presentation: "transparentModal",
+            presentation: "card",
             animation: 'fade',
-            gestureEnabled: true,
+            gestureEnabled: false,
             headerTitle: () => null, headerTintColor: "#FFF",
             headerStyle: { backgroundColor: "#0D0E10", height: windowHeight / 10 },
           }} />
           <Stack.Screen name="Global Hits Movies" component={GlobalHitsMovies} options={{
-            presentation: "transparentModal",
+            presentation: "card",
             animation: 'fade',
-            gestureEnabled: true,
+            gestureEnabled: false,
             headerTitle: () => null, headerTintColor: "#FFF",
             headerStyle: { backgroundColor: "#0D0E10", height: windowHeight / 10 },
           }} />
           <Stack.Screen name="Romantic Movies" component={RomanticMovies} options={{
-            presentation: "transparentModal",
+            presentation: "card",
             animation: 'fade',
-            gestureEnabled: true,
+            gestureEnabled: false,
             headerTitle: () => null, headerTintColor: "#FFF",
             headerStyle: { backgroundColor: "#0D0E10", height: windowHeight / 10 },
           }} />
           <Stack.Screen name="South Dubbed Movies" component={SouthDubbedMovies} options={{
-            presentation: "transparentModal",
+            presentation: "card",
             animation: 'fade',
-            gestureEnabled: true,
+            gestureEnabled: false,
             headerTitle: () => null, headerTintColor: "#FFF",
             headerStyle: { backgroundColor: "#0D0E10", height: windowHeight / 10 },
           }} />
           <Stack.Screen name="Bhojpuri Bhaukal" component={BhojpuriBhaukalMovies} options={{
-            presentation: "transparentModal",
+            presentation: "card",
             animation: 'fade',
-            gestureEnabled: true,
+            gestureEnabled: false,
             headerTitle: () => null, headerTintColor: "#FFF",
             headerStyle: { backgroundColor: "#0D0E10", height: windowHeight / 10 },
           }} />
           <Stack.Screen name="Horror" component={HorrorMovies} options={{
-            presentation: "transparentModal",
+            presentation: "card",
             animation: 'fade',
-            gestureEnabled: true,
+            gestureEnabled: false,
             headerTitle: () => null, headerTintColor: "#FFF",
             headerStyle: { backgroundColor: "#0D0E10", height: windowHeight / 10 },
           }} />
           <Stack.Screen name="MoviePlayer" component={MoviePlayer}
             options={{
-              presentation: "transparentModal",
+              presentation: "card",
               animation: 'fade',
-              gestureEnabled: true,
+              gestureEnabled: false,
               headerShown: false, cardStyle: { backgroundColor: "#0D0E10", width: "100%", height: "100%" }
             }} />
           <Stack.Screen name="LiveStreaming" component={LiveStreaming}
             options={{
-              presentation: "transparentModal",
+              presentation: "card",
               animation: 'fade',
-              gestureEnabled: true,
+              gestureEnabled: false,
               headerTitle: () => null, headerTintColor: "#FFF",
               headerStyle: { backgroundColor: "#0D0E10", height: windowHeight / 10 },
             }} />
