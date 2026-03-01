8e8e4c051aa6854814ed77dd58553855ec27bff6
shashank <singhshashank081@gmail.com>
2026-03-01 13:00:54 +0530

    updaed pip for aito handle

--- plugins/withPipBackHandler.js
+++ plugins/withPipBackHandler.js
@@ -1,12 +1,7 @@
-const {
-  withAndroidManifest,
-  withMainActivity,
-} = require("@expo/config-plugins");
+const { withAndroidManifest } = require("@expo/config-plugins");
 
-module.exports = function withPipBackHandler(config) {
-
-  // ---------- Enable PiP ----------
-  config = withAndroidManifest(config, config => {
+module.exports = function withPip(config) {
+  return withAndroidManifest(config, config => {
     const activity =
       config.modResults.manifest.application[0].activity.find(
         a => a.$["android:name"] === ".MainActivity"
@@ -14,45 +9,8 @@ module.exports = function withPipBackHandler(config) {
 
     activity.$["android:supportsPictureInPicture"] = "true";
     activity.$["android:resizeableActivity"] = "true";
+    activity.$["android:launchMode"] = "singleTask";
 
     return config;
   });
-
-  // ---------- Modify MainActivity.kt safely ----------
-  config = withMainActivity(config, config => {
-    let contents = config.modResults.contents;
-
-    // add import once
-    if (!contents.includes("android.app.PictureInPictureParams")) {
-      contents = contents.replace(
-        "import android.os.Bundle",
-        `import android.os.Bundle
-import android.os.Build
-import android.app.PictureInPictureParams`
-      );
-    }
-
-    // override onBackPressed safely
-    if (!contents.includes("onBackPressed")) {
-      contents = contents.replace(
-        "super.onCreate(null)",
-        `super.onCreate(null)
-
-    override fun onBackPressed() {
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            enterPictureInPictureMode(
-                PictureInPictureParams.Builder().build()
-            )
-        } else {
-            super.onBackPressed()
-        }
-    }`
-      );
-    }
-
-    config.modResults.contents = contents;
-    return config;
-  });
-
-  return config;
 };
